c32a6ed766359b2b5cf8e0bc0966326b39af2381
Kim Milán <milan.knp@gmail.com>
2020-09-27 13:38:45 +0300

    Updates

---
+++ precache-manifest.bd3c6a11f0c39ead56d1c99df0e77a01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a254257a82edf73098348d07e030ff",
+    "url": "/silverstreaming/index.html"
+  },
+  {
+    "revision": "fe79ae84c92f1fa3f11b",
+    "url": "/silverstreaming/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e6853125f923e4fbf5bd",
+    "url": "/silverstreaming/static/js/2.184bcf9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/silverstreaming/static/js/2.184bcf9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe79ae84c92f1fa3f11b",
+    "url": "/silverstreaming/static/js/main.ce699b38.chunk.js"
+  },
+  {
+    "revision": "46081ff646ea648a9a44",
+    "url": "/silverstreaming/static/js/runtime-main.b73a4a18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/silverstreaming/static/media/logo.5d5d9eef.svg"
+  }
+]);
