f449c3e6cf98320a5aa7b8cbabe4eb60d1930f14
Ville Mannila <ville.mannila@tuni.fi>
2020-10-10 15:55:10 +0300

    Added Token generation to landingContainer.js

--- src/Containers/LandingContainer.js
+++ src/Containers/LandingContainer.js
@@ -6,9 +6,25 @@ export default function LandingContainer() {
 
     const [tokenId, setTokenId] = useState("");
   
+        function generateToken(){
+            var token = '';
+            var chars = "ABCDEFGHJKLMNPQRSTUVXYZabcdefghijklmnopqrstuvwxyz23456789"
+            var charlen = chars.length;
+            var pointer = 0;
+            for (var i = 0; i < 12; i++){
+                pointer = Math.floor(Math.random()*charlen);
+                token += chars.charAt(pointer);
+            }
+            var inputfield = document.getElementById("tokenIdInput")
+            inputfield.value = token;
+        }
+
     return (
       <div className="container">
           <div className="landingInputBox">
+            <div classname="tokenGenerator">
+                <button onClick={generateToken}>Generate token</button>
+            </div>
             <div className="input">
                 <label htmlFor="#tokenIdInput">Token</label>
                 <input id="tokenIdInput" onChange={event => setTokenId(event.target.value)} placeholder="Input token id"/>
